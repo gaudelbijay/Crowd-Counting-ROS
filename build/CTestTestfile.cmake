1878e5356c697d44e79a68923228dc8746a109d4
gaudelbijay <gaudelbijay10@gmail.com>
2022-01-24 20:55:53 +0545

    .gitignore is working now

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gaudel/Desktop/crowd_counting/src
-# Build directory: /home/gaudel/Desktop/crowd_counting/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("crowd_counting_pkg")
--- build/crowd_counting_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gaudel/Desktop/crowd_counting/src/crowd_counting_pkg
-# Build directory: /home/gaudel/Desktop/crowd_counting/build/crowd_counting_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
